c78485f2a0c346b9846927c9700b43f2aa6f2eec
luongnb <tienluong392003@gmail.com>
2022-12-14 21:46:44 +0700

    luong dep trai

---
+++ luong/admin.php
@@ -0,0 +1,29 @@
+<?php
+include 'view/header.php';
+
+//include dao để dùng các functione: 
+include "model/pdo.php";
+include "model/loai.php";
+include "model/sanpham.php";
+include "model/nguoidung.php";
+include "model/hoadon.php";
+// controller
+if (isset($_GET['act'])) {
+    $act = $_GET['act'];
+    switch ($act) {
+            // CONTROLLER LOẠI:
+        case "addl":
+            if(isset($_POST['btn_add']) && ($_POST['btn_add'])) { 
+                $name_cate = $_POST['name_cate'];
+                them_loai($name_cate);
+                echo '<script>alert("Thêm loại thành công!")</script>';
+            }
+            include "view/loai/add.php";
+            break;
+        case "listl":
+            $ds_loai = loadall_loai();
+            include "view/loai/list.php";
+            break;
+        case "sualoai": 
+            if(isset($_GET['id_cate']) && ($_GET['id_cate'] > 0)) { 
+                $id_cate = $_GET['id_cate'];
