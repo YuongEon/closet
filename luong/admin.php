6fe1d20aa9dc4480737d6c1077cde5abf3517343
luongnb <tienluong392003@gmail.com>
2022-12-14 22:31:30 +0700

    luongdeptrai

--- luong/admin.php
+++ luong/admin.php
@@ -20,10 +20,15 @@ if (isset($_GET['act'])) {
             }
             include "view/loai/add.php";
             break;
-        case "listl":
-            $ds_loai = loadall_loai();
-            include "view/loai/list.php";
-            break;
-        case "sualoai": 
-            if(isset($_GET['id_cate']) && ($_GET['id_cate'] > 0)) { 
-                $id_cate = $_GET['id_cate'];
+            case "addl":
+                if(isset($_POST['btn_add']) && ($_POST['btn_add'])) { 
+                    $name_cate = $_POST['name_cate'];
+                    them_loai($name_cate);
+                    echo '<script>alert("Thêm loại thành công!")</script>';
+                }
+                include "view/loai/add.php";
+                break;
+            case "listl":
+                $ds_loai = loadall_loai();
+                include "view/loai/list.php";
+                break;    
